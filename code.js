440c3da3f31004d4f036f622005a5b35a3b92763
Fleapous <girayegecan02@gmail.com>
2022-05-23 22:13:16 +0200

    divs can display on the canvas now but there is bugs

--- code.js
+++ code.js
@@ -1,6 +1,18 @@
 
 function canvasCreator(size){
+    let canvas = document.querySelector('.canvas')
 
+    /*removing the old canvas */
+    while (canvas.firstChild){
+        canvas.removeChild(canvas.firstChild);
+    }
+
+    /* Creating the new colors */
+    for (let i = 0; i < size * size; i++){
+        let canvasPiece = document.createElement('div')
+        canvasPiece.classList.add('piece');
+        canvas.appendChild(canvasPiece);
+    }
 }
 
 let value = document.createElement('div');
@@ -10,6 +22,8 @@ let slider = document.querySelector('#canvas-size');
 slider.addEventListener('input', function (){
     value.textContent = this.value;
     control.appendChild(value);
+    canvasCreator(this.value)
 })
+
 
 
